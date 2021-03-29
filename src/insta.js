2498774ae4de9c3baca6cfe46007f23aead998e0
headMade_Web <headMade@pro.ru>
2021-03-29 10:05:24 +0300

    insta integr

--- src/insta.js
+++ src/insta.js
@@ -1,20 +1,11 @@
 import React from "react";
 import InstagramEmbed from 'react-instagram-embed';
+import { FacebookProvider, EmbeddedPost } from 'react-facebook';
 
 export default function App() {
     return (
-        <InstagramEmbed
-            url='https://www.instagram.com/p/CM4bRpJjDcE/?utm_source=ig_web_copy_link'
-            clientAccessToken='1560234874366509|1c8bf169d4fd8c2c44fdada12e0292ef'
-            maxWidth={600}
-            hideCaption={false}
-            containerTagName='div'
-            protocol=''
-            injectScript
-            onLoading={() => {}}
-            onSuccess={() => {}}
-            onAfterRender={() => {}}
-            onFailure={() => {}}
-        />
+        <FacebookProvider appId="1560234874366509">
+            <EmbeddedPost href="https://www.facebook.com/fit4brain/posts/4061288663931711" width="500" />
+        </FacebookProvider>
     )
 }
