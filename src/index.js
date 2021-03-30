4da80b149edf2c53b69ee0cbd6cfd345234de87b
headMade_Web <headMade@pro.ru>
2021-03-30 10:39:07 +0300

    add twitter widget

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./insta";
+import App from "./twitter";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
---
+++ src/twitter.js
@@ -0,0 +1,8 @@
+import { Tweet } from 'react-twitter-widgets';
+import React from "react";
+
+export default function Twitter() {
+    return (
+        <Tweet tweetId="1376129545112260610" />
+    )
+}
